efa7a8d3f4275b4694c031fa12917f9a90f0ab65
Viacheslav <dphusevvia4eslav@gmail.com>
2021-06-12 00:40:36 +0300

    закончен адаптив главного слайдера

--- src/js/swiper/swiper.js
+++ src/js/swiper/swiper.js
@@ -1,5 +1,22 @@
 export function mainBannerSwiper() {
    const BannerSlider = new Swiper('.banner-slider__container', {
-
+      keyboard: {
+         // Управление стрелками
+         onlyInViewport: true,
+      },
+      spaceBetween: 10,
+      pagination: {
+         el: '.bunner-slider__pagination',
+         type: 'bullets',
+         clickable: true,
+         // активный кружок (большой) "Библиотека"
+         dynamicBullets: true,
+         // к-во активных кружков (больших) "Библиотека"
+         dynamicMainBullets: 1,
+      },
+      navigation: {
+         nextEl: '.bunner-slider__button-next',
+         prevEl: '.bunner-slider__button-prev ',
+      },
    });
 }
